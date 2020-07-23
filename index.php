e9638e82b1dadaff18b20d8328131252be9c00c0
Jorge Felix Gonzalez <jorgefelix1920@hotmail.com>
2020-07-23 12:17:57 -0400

    Se agrego el inicio de sesión.

--- index.php
+++ index.php
@@ -5,21 +5,36 @@
  *  Funcion:    Corrección de tarea 7 de luego de verificar que la misma no cumplía con lo solicitado por el maestro 
 */
 ?>
- <html lang="en">
-        <?php 
-        // llama el header
-        include('pages/header.php');?>
-<body>
-    
-<?php 
-        // llama el footer
-        include('pages/footer.php');?>
+<html lang="en">
+<?php
+// llama el header
+include 'pages/header.php'; ?>
+
+<body class="text-center">
+    <form class="form-signin">
+        <img class="mb-4" src="../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Inicia Sesión</h1>
+        <label for="inputEmail" class="sr-only">Email</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me">Recordar Usuario 
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+    </form>
+
+
+
+
+
+
+    <?php
+    // llama el footer
+    include('pages/footer.php'); ?>
 </body>
+
 </html>
-
-
-
-
-
-
-
--- pages/header.php
+++ pages/header.php
@@ -1,7 +1,16 @@
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="Tarea 7 Restruturada" content="">
+    <meta name="author" content="Jorge Felix, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.0.1">
+    
+    <!-- Estilos CSS-->
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+    <link href="../assets/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="../css/signin.css" rel="stylesheet">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
+    <!-- Bootstrap core CSS -->
     <title>New Task 7</title>
 </head>
