28aab3e2aa6ab92bc57446a33e9ad4abac09ea8b
Jorge Felix Gonzalez <jorgefelix1920@hotmail.com>
2020-07-23 11:44:24 -0400

    Tarea 7 Restaurada de luego de verificar que la misma no cumplía con lo solicitado por el maestro

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<? 
+/** Nombre:     Jorge Felix Gonzalez 
+ *  Fecha:      23/07/2020
+ *  Funcion:    Corrección de tarea 7 de luego de verificar que la misma no cumplía con lo solicitado por el maestro 
+*/
+?>
+ <html lang="en">
+        <?php 
+        // llama el header
+        include('pages/header.php');?>
+<body>
+    
+<?php 
+        // llama el footer
+        include('pages/footer.php');?>
+</body>
+</html>
+
+
+
+
+
+
+
